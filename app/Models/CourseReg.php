0c88bb62cbcdfca0d43141896cd7073febf5ead7
Agbeja Joshua <talktogbenga@gmail.com>
2022-02-23 16:52:19 +0100

    Added GraphQl Query for Profiles...

--- app/Models/CourseReg.php
+++ app/Models/CourseReg.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CourseReg extends Model
 {
     use HasFactory;
+
+    public function course(): BelongsTo
+    {
+        return $this->belongsTo(Course::class, 'courseId');
+    }
+    
+    public function profile(): BelongsTo
+    {
+        return $this->belongsTo(Profile::class, 'profileId');
+    }
+   
 }
--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Profile extends Model
 {
     use HasFactory;
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class, 'roleId');
+    }
+
+    public function courses(): HasMany
+    {
+        return $this->hasMany(CourseReg::class,'profileId');
+    }
+
 }
